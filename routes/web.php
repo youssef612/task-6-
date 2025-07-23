18e2d438113e5abe549b7a92793e304a8747495a
Youssef <youssefsawan35@gemail.com>
2025-07-23 05:30:32 +0300

    uploading the new files

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hello';
 });
 
 Route::get('/hello',function() {
     return 'hello';
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
